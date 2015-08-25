e05280d86c07ab21090e9e52dc6f93ca76fa2175
3Thieves <SalamanderTheGreatest@gmail.com>
2015-08-25 14:11:44 -0500

    Reworked giveMonies, , added Inequality happiness decay, changes to UI.

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 //this will be the mechanics for Sam's and mybb
     var chance;
     var difficulty;
+	var week = 0;
     var userWealth = 25;
-    var business_Sector = {machine:1, wealth:75, resource:0, happy:37.5};//neo-toyoko
-    var manufacturing_Sector = {machine:1, wealth:50, resource:0, happy:25};//CosmoFerguson
-    var labor_Sector = {machine:1, wealth:25, resource:0, happy:12.5};//Luna-Ohio
+    var business_Sector = {machine:1, wealth:60, resource:0, happy:80, name: "Neo-Tokyo"};//neo-toyoko
+    var manufacturing_Sector = {machine:1, wealth:35, resource:0, happy:60, name: "CosmoFerguson"};//CosmoFerguson
+    var labor_Sector = {machine:1, wealth:25, resource:0, happy:50, name: "Luna-Ohio"};//Luna-Ohio
+	var country = [labor_Sector, manufacturing_Sector, business_Sector];
 	var acted = false;
-
+	var statements = "";
     
     //primary();
     var randomEvent = function(chance){
@@ -14,15 +16,53 @@
         eventThatHappens(Math.floor(3*Math.random()+1));
     };
     
+	function tellPlayer(string){
+	statements += string + "\n \n";
+	}
+	
+	function wealthGap(upperSector, lowerSector){ // Calculates Wealth Gap and effects happiness accordingly
+		var wealthGap = Math.floor((upperSector.wealth-lowerSector.wealth)/lowerSector.wealth);
+		if(wealthGap > -1*wealthGap){ //Ensures no gain in happiness from inequality
+			lowerSector.happy -= wealthGap;
+			tellPlayer(lowerSector.name + " is unhappy about the wealth gap between them and " + upperSector.name);
+			}
+	};
+	
+	function checkMinMax(sector){ // Makes sure that no sectors have negative values or those that exceed the bounds
+		for (key in sector){
+			if(!isNaN(sector[key])){
+				if(sector[key] < 0){
+					sector[key] = 0;
+				}else if(  key === "happy" && sector[key] > 100){
+					sector[key] = 100;
+				}
+			}
+		}
+	};
+	
     var passTime = function(numberOfTurns){
+		week++;
+		statements = "";
+		
         labor_Sector.resource+=labor_Sector.happy*labor_Sector.machine*numberOfTurns/10;
         manufacturing_Sector.resource+=labor_Sector.resource*manufacturing_Sector.happy*manufacturing_Sector.machine*numberOfTurns/20;
         labor_Sector.resource = 0;
         business_Sector.resource+= manufacturing_Sector.resource*business_Sector.happy*business_Sector.machine*numberOfTurns/20;
         manufacturing_Sector.resource = 0;
-        business_Sector.wealth += business_Sector.resource*3/8;     
-        manufacturing_Sector.wealth += business_Sector.resource*2/8;
-        labor_Sector.wealth += business_Sector.resource*1/8;
+        business_Sector.wealth += business_Sector.resource*5.25/10;     
+        manufacturing_Sector.wealth += business_Sector.resource*2.5/10;
+        labor_Sector.wealth += business_Sector.resource*1/10;
+		
+		//Wealth Gap effects happiness
+		wealthGap(manufacturing_Sector, labor_Sector);
+		wealthGap(business_Sector, manufacturing_Sector);
+			
+		// Ensures no Negative Values for Sector resources
+		checkMinMax(business_Sector);
+		checkMinMax(manufacturing_Sector);
+		checkMinMax(labor_Sector);
+		
+		
        // userWealth += business_Sector.resource*2/8;
         business_Sector.resource =0;//all that stuff above does da resources
         //does a randomEvent test for every turn the user advances.
@@ -39,7 +79,7 @@
     };
 	
 	function show(){ // Updates Numbers in HTML to those in the program.
-		document.getElementById("Funds").innerHTML = "Current Funds:" + Math.floor(userWealth);
+		document.getElementById("Funds").innerHTML ="Current Funds: " + Math.floor(userWealth) + "<br> Week: " + week;
 		
 		document.getElementById("HighWealth").innerHTML = Math.floor(business_Sector.wealth);
 		document.getElementById("HighHappy").innerHTML = business_Sector.happy.toFixed(2);
@@ -52,155 +92,62 @@
 		document.getElementById("LowWealth").innerHTML = Math.floor(labor_Sector.wealth);
 		document.getElementById("LowHappy").innerHTML = labor_Sector.happy.toFixed(2);
 		document.getElementById("LowMachine").innerHTML = labor_Sector.machine;
+		
+		document.getElementById("status").innerHTML = statements;
 		console.log("Updated");
 		
 		};
     
-    var moveMonies = function(amount, city){//need to prompt user before hand for city name and amount, in the method call, if it was triggered by the tax method make it negative, if it was taken by the grant button, leave it positive.
-        city = city.toString().charAt(0);//thinks this will limit it to only the 1st char
-        city = city.toUpperCase();
-        var limit;
-		var updatedCity = true;
-        switch (city){
-            case "N":
-            	var initialWealth =business_Sector.wealth;
-				console.log("Case read in as N");
-                if(amount > 0)//giving money
-                {
-                	if(amount < userWealth){
-            		business_Sector.wealth +=amount;
-                	}
-                	else{
-                		amount = userWealth;
-                		business_Sector.wealth +=amount;
-                	}
-                }
-                else//taking money, based off the assumption no one would waste their time with 0 transactions.
-                {
-                	limit = business_Sector.wealth;
-                	if(limit < (-1 * amount)){
-                		business_Sector.wealth -=limit;
-                		amount = limit;
-                	}
-                	else{
-                		business_Sector.wealth +=amount;
-                	}
-                }
-                if(business_Sector.wealth < initialWealth)//they lost monies
-                {
-                	business_Sector.happy -= 1 + 50*(1-business_Sector.wealth/initialWealth);
-                	if(business_Sector.happy < 0)
-                	{
-                		business_Sector.happy = 0;
-                	}
-                }
-                else//they make monies
-                {
-                	business_Sector.happy += 25*(1-initialWealth/business_Sector.wealth);
-                	if(business_Sector.happy >100)
-                	{
-                		business_Sector.happy = 100;
-                	}
-                }
-            break;
-            case "C":
-                var initialWealth =manufacturing_Sector.wealth;
-				console.log("Case read in as C");
-                if(amount > 0)//giving money
-                {
-                	if(amount < userWealth){
-            		manufacturing_Sector.wealth +=amount;
-                	}
-                	else{
-                		amount = userWealth;
-                		manufacturing_Sector.wealth +=amount;
-                	}
-                }
-                else//taking money, based off the assumption no one would waste their time with 0 transactions.
-                {
-                	limit = manufacturing_Sector.wealth;
-                	if(limit < -1*amount){
-                		manufacturing_Sector.wealth -=limit;
-                		amount = limit;
-                	}
-                	else{
-                		manufacturing_Sector.wealth +=amount;
-                	}
-                }
-                if(manufacturing_Sector.wealth < initialWealth)//they lost monies
-                {
-                	manufacturing_Sector.happy -= 1 + 50*(1-manufacturing_Sector.wealth/initialWealth);
-                	if(manufacturing_Sector.happy < 0)
-                	{
-                		manufacturing_Sector.happy = 0;
-                	}
-                }
-                else//they make monies
-                {
-                	manufacturing_Sector.happy += 25*(1-initialWealth/manufacturing_Sector.wealth);
-                	if(manufacturing_Sector.happy >100)
-                	{
-                		manufacturing_Sector.happy = 100;
-                	}
-                }
-            break;
-            case "L"://just have to fix which sector it goes to .
-                var initialWealth =labor_Sector.wealth;
-				console.log("Case read in as L");
-                if(amount > 0)//giving money
-                {
-                	if(amount < userWealth){
-            		labor_Sector.wealth +=amount;
-                	}
-                	else{
-                		amount = userWealth;
-                		labor_Sector.wealth +=amount;
-                	}
-                }
-                else//taking money, based off the assumption no one would waste their time with 0 transactions.
-                {
-                	limit = labor_Sector.wealth;
-                	if(limit < -1*amount){
-                		labor_Sector.wealth -=limit
-                		amount = limit;
-                	}
-                	else{
-                		labor_Sector.wealth +=amount;
-                	}
-                }
-                if(labor_Sector.wealth < initialWealth)//they lost monies
-                {
-                	labor_Sector.happy -= 1 + 50*(1-labor_Sector.wealth/initialWealth);
-                	if(labor_Sector.happy < 0)
-                	{
-                		labor_Sector.happy = 0;
-                	}
-                }
-                else//they make monies
-                {
-                	labor_Sector.happy += 25*(1-initialWealth/labor_Sector.wealth);
-                	if(labor_Sector.happy >100)
-                	{
-                		labor_Sector.happy = 100;
-                	}
-                }
-            break;
-            
-            default:
-				console.log("Nothing read in");
-				updatedCity = false;
-				break;
+	var moveMonies = function(amount, city){
+		if (acted){
+			alert("You have already acted this turn");
+			return;
+		}
+		city = city.toString().charAt(0); // Shortens user input to make it match up easy
+		city = city.toUpperCase();
+		for(var i = 0; i < country.length; i++){ // searches through country for name of Sector
+			if( country[i].name.charAt(0) === city){
+				var initialWealth = country[i].wealth; //saves the initial wealth of the sector for happiness calculations
+			
+				if(amount > userWealth){ // Makes sure the player doesn't give more money than they have.
+						amount = userWealth;
+				}else if(-1 * amount > country[i].wealth){//Makes sure you don't take more money from the sector than they have 
+						amount = -1 * country[i].wealth;
+				}
 				
+				if ( amount > 0){ // Giving money to a city,
+
+                	country[i].wealth += amount; //adjusts amount of money the sector has
+					userWealth -= amount; // and how much the player has.
+					
+					country[i].happy += 25*(1- initialWealth/country[i].wealth); //Makes the target happier, they got money!
+					acted = true;
+					
+				} else if (amount < 0){ // Taking money from a city
+					
+					country[i].wealth += amount; // takes money from the city (adds a negative number)
+					userWealth -= amount; //and gives to the player
+					
+					country[i].happy -= 1 + 60*(1-country[i].wealth/initialWealth); // Takes away their happiness, taxing tends to do that.
+					acted = true;
+				} else{
+					alert("No money was transferred, you can still act this turn.");
+				}
+				checkMinMax(country[i])
+					
+			
+			}else if (i === country.length - 1 && !acted){
+				alert("No such city was found");
 			}
-			if(updatedCity){
-			userWealth -=amount;
-			acted = true;
-			}
-        };
-		
+		}
+	};
 		var grant = function(){
 		if(!acted)
 		{
+			if (userWealth < 1){
+				alert("You do not have any money to give!");
+				return;
+			}
 			var inpCity = prompt("Enter City Name");
 			var inpAmount = parseInt(prompt("Enter Amount to Give"));
 			console.log("GrantRunning");
